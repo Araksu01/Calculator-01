fd7a5fb5cbb6aeda178b3f37d4cc4108e7f40dcc
Araksu01 <143333598+Araksu01@users.noreply.github.com>
2023-08-28 19:17:28 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class Main {
-
-    public static void main(String[] args) throws IOException  {
+    public static String calc(String input) {
         System.out.println("Для вычисления введите исходные данные в следующем формате: \"a_c_b\", где \"a\" и \"b\" - целые числа от 1 до 10 включительно, \"с\" - знак операции сложения, вычитания, умножения или деления, \"_\" - пробел.)");
         Scanner scan = new Scanner(System.in);
-        String input_data = scan.nextLine();
+        return scan.nextLine();
+    }
+    public static void main(String[] args) throws IOException  {
+        String input_data;
+        input_data = calc(null);
         String regex1 = "\\d?\\d\\s\\W\\s\\d\\d?";
         Pattern pattern1 = Pattern.compile(regex1);
         Matcher matcher1 = pattern1.matcher(input_data);
@@ -25,36 +27,36 @@ class Main {
             for (int i = 0; i < numbers.length; i++) {
                 numArr[i] = Integer.parseInt(numbers[i]);
             }
-                int a;
-                a = numArr[0];
-                int b;
-                b = numArr[1];
-                if (a < 1 || a > 10 || b < 1 || b > 10) {
-                    throw new IOException("Могут исользоваться только целые числа от 1 до 10 включительно");
-                } else {
-                    char op;
-                    op = operation.charAt(0);
-                    switch (op) {
-                        case '+':
-                            System.out.println("Результат = " + (a + b));
-                            break;
-                        case '-':
-                            System.out.println("Результат = " + (a - b));
-                            break;
-                        case '*':
-                            System.out.println("Результат = " + (a * b));
-                            break;
-                        case '/':
-                            System.out.println("Результат = " + (a / b));
-                            break;
-                        default:
-                            throw new IOException("Некорректно указана операция.");
+            int a;
+            a = numArr[0];
+            int b;
+            b = numArr[1];
+            if (a < 1 || a > 10 || b < 1 || b > 10) {
+                throw new IOException("Могут исользоваться только целые числа от 1 до 10 включительно");
+            } else {
+                char op;
+                op = operation.charAt(0);
+                switch (op) {
+                    case '+':
+                        System.out.println("Результат = " + (a + b));
+                        break;
+                    case '-':
+                        System.out.println("Результат = " + (a - b));
+                        break;
+                    case '*':
+                        System.out.println("Результат = " + (a * b));
+                        break;
+                    case '/':
+                        System.out.println("Результат = " + (a / b));
+                        break;
+                    default:
+                        throw new IOException("Некорректно указана операция.");
 
-                    }
                 }
+            }
         }
-
-
-
     }
+
 }
+
+
